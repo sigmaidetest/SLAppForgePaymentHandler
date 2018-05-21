2c0d1371570e2067b0dd2bd2808d8cc6ac93a9c4
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-05-21 15:12:15 +0530

    wdw

--- SLAppForgePaymentHandler/lambda.js
+++ SLAppForgePaymentHandler/lambda.js
@@ -1,10 +1,21 @@
 let AWS = require('aws-sdk');
 let axios = require('axios');
+let ses = new AWS.SES();
 
 const PAYPAL_API_BASE_URL = 'https://api.sandbox.paypal.com';
 const CLIENT_KEY = process.env['PP_CLIENT_KEY'];
 const CLIENT_SECRET = process.env['PP_CLIENT_SECRET'];
 const PAY_AMOUNT = '1';
+
+const NOTIFICATION_SENDING_EMAIL = 'udith@adroitlogic.com';
+const NOTIFICATION_RECEIVING_EMAIL = 'udith+paypal@adroitlogic.com';
+
+const ERROR_CODES = {
+	FAILED_PAYMENT_CREATION: 1,
+	FAILED_PAYMENT_EXECUTION: 2,
+	FAILED_PAYMENT_APPROVAL: 3,
+	FAILED_ACCOUNT_UPGRADE: 4
+};
 
 exports.handler = function (event, context, callback) {
 
@@ -88,58 +99,65 @@ function createPayment(data, callback) {
 						});
 					} else {
 						console.log('Payment ID is not available');
-						sendFailureResponse(callback);
+						sendFailureResponse(callback, ERROR_CODES.FAILED_PAYMENT_CREATION);
 					}
 				})
 				.catch(err => {
 					console.log('Failed to create payment', err);
-					sendFailureResponse(callback);
+					sendFailureResponse(callback, ERROR_CODES.FAILED_PAYMENT_CREATION);
 				});
 		})
 		.catch((err) => {
 			console.log('Failed to get access token', err);
-			sendFailureResponse(callback);
+			sendNotificationEmail('Sigma - Payment Creation Failed', `Payment creation failed for ${username} due to ${err.message}`);
+			sendFailureResponse(callback, ERROR_CODES.FAILED_PAYMENT_CREATION);
 		});
 }
 
 function executePayment(data, callback) {
 
 	let paymentId = data['paymentId'];
-    let payertId = data['payerId'];
+	let payertId = data['payerId'];
+	let username = data['username']
 
 	console.log('Executing payment', paymentId, payertId);
 
-    getAccessToken()
-        .then(accessToken => {
-            console.log(accessToken);
-            axios.post(`${PAYPAL_API_BASE_URL}/v1/payments/payment/${paymentId}/execute`,
-                {
-                    'payer_id': payertId
-                },
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${accessToken}`
-                    }
-                })
-                .then(payPalResponse => {
-                    let data = payPalResponse.data;
+	getAccessToken()
+		.then(accessToken => {
+			console.log(accessToken);
+			axios.post(`${PAYPAL_API_BASE_URL}/v1/payments/payment/${paymentId}/execute`,
+				{
+					'payer_id': payertId
+				},
+				{
+					headers: {
+						'Content-Type': 'application/json',
+						'Authorization': `Bearer ${accessToken}`
+					}
+				})
+				.then(payPalResponse => {
+					let data = payPalResponse.data;
 					if (data['state'] === "approved") {
 						console.log('Successfully executed payment', data);
+						sendNotificationEmail('Sigma - Payment Received', `Successfully received payment from ${username}`);
+
 						sendSuccessResponse(callback, {});
 					} else {
 						console.log('Payment execution failed', data);
-						sendFailureResponse(callback);
+						sendNotificationEmail('Sigma - Payment Failed', `Payment not approved from ${username}`);
+						sendFailureResponse(callback, ERROR_CODES.FAILED_PAYMENT_APPROVAL);
 					}
-                })
-                .catch(err => {
-                    console.log('Failed to execute payment', paymentId, payertId, err);
-					sendFailureResponse(callback);
-                });
-        })
-        .catch((err) => {
+				})
+				.catch(err => {
+					console.log('Failed to execute payment', paymentId, payertId, err);
+					sendNotificationEmail('Sigma - Payment Failed', `Payment failed from ${username} due to ${err.message}`);
+					sendFailureResponse(callback, ERROR_CODES.FAILED_PAYMENT_EXECUTION);
+				});
+		})
+		.catch((err) => {
 			console.log('Failed to get access token', err);
-			sendFailureResponse(callback);
+			sendNotificationEmail('Sigma - Payment Failed', `Payment failed from ${username} due to ${err.message}`);
+			sendFailureResponse(callback, ERROR_CODES.FAILED_PAYMENT_EXECUTION);
 		});
 }
 
@@ -147,8 +165,8 @@ function sendSuccessResponse(callback, data) {
 	sendResponse(callback, data, 200);
 };
 
-function sendFailureResponse(callback) {
-	sendResponse(callback, {}, 500);
+function sendFailureResponse(callback, errCode) {
+	sendResponse(callback, {error: errCode}, 500);
 };
 
 function sendResponse(callback, data, statusCode) {
@@ -160,4 +178,36 @@ function sendResponse(callback, data, statusCode) {
 		},
 		"body": JSON.stringify(data)
 	});
+};
+
+function sendNotificationEmail(emailSubject, emailBodyText) {
+	let params = {
+		Destination: {
+			ToAddresses: [NOTIFICATION_RECEIVING_EMAIL]
+		},
+		Message: {
+			Body: {
+				Text: {
+					Data: emailBodyText,
+					Charset: 'UTF-8'
+				}
+			},
+			Subject: {
+				Data: emailSubject,
+				Charset: 'UTF-8'
+			}
+		},
+		Source: NOTIFICATION_SENDING_EMAIL
+	};
+	ses.sendEmail(params).promise()
+	.then(data => {
+		console.log('Notification email sent', emailSubject);
+	})
+	.catch(err => {
+		console.log("Failed to send notification email", err);
+	});
+}
+
+function updateUser(username) {
+
 };
